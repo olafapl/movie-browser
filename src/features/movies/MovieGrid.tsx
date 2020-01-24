c6152d6842f4779bbdb7a7bc30259e329cf6589a
Olaf Apelseth Liadal <olaf.liadal@gmail.com>
2020-01-25 00:25:48 +0100

    Extract movie grid to separate component

---
+++ src/features/movies/MovieGrid.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import css from "@emotion/css/macro";
+import {
+  Flex,
+  Spinner,
+  Box,
+  Grid,
+  Stack,
+  Button,
+  PseudoBox,
+  Link,
+  Text
+} from "@chakra-ui/core";
+import { Link as RouterLink } from "react-router-dom";
+import TmdbImage from "components/TmdbImage";
+
+interface MovieGridProps {
+  movies: Tmdb.MovieResult[] | null;
+  page: number;
+  setPage: (page: number) => void;
+  showPreviousButton: boolean;
+  showNextButton: boolean;
+  isLoading: boolean;
+  error: string | null;
+}
+
+const MovieGrid: React.FC<MovieGridProps> = ({
+  movies,
+  page,
+  setPage,
+  showPreviousButton,
+  showNextButton,
+  isLoading,
+  error
+}) => {
+  return (
+    <React.Fragment>
+      {movies?.length ? (
+        <Stack spacing="4">
+          <Grid
+            gridTemplateColumns={[
+              "repeat(2, 1fr)",
+              "repeat(4, 1fr)",
+              "repeat(5, 1fr)"
+            ]}
+            gap="4"
+            alignItems="flex-start"
+          >
+            {movies.map(movie => (
+              <Link
+                // @ts-ignore
+                as={RouterLink}
+                to={`/movie/${movie.id}`}
+                key={movie.id}
+                pos="relative"
+                height={movie.poster_path ? "unset" : "100%"}
+                width="100%"
+                borderRadius="md"
+                overflow="hidden"
+                _hover={{ textDecor: "none" }}
+              >
+                <PseudoBox
+                  role="group"
+                  height="100%"
+                  width="100%"
+                  boxShadow="md"
+                  transition="opacity 0.2s ease-in-out"
+                  _hover={{ opacity: 0.8 }}
+                >
+                  {movie.poster_path ? (
+                    <TmdbImage
+                      path={movie.poster_path}
+                      imageType="poster"
+                      sizes="(max-width: 30em) 50vw, (max-width: 48em) 25vw, 20vw"
+                      css={css`
+                        width: 100%;
+                      `}
+                    />
+                  ) : (
+                    <Box
+                      height="100%"
+                      width="100%"
+                      backgroundColor="gray.700"
+                    ></Box>
+                  )}
+                  <PseudoBox
+                    pos="absolute"
+                    transform="translatey(1rem)"
+                    right="0"
+                    bottom="0"
+                    left="0"
+                    p="2"
+                    pt="4"
+                    backgroundImage="linear-gradient(to top, #000, transparent)"
+                    opacity={0}
+                    transition="transform 0.2s ease-in-out, opacity 0.2s ease-in-out"
+                    _groupHover={{ transform: "translatey(0)", opacity: 1 }}
+                  >
+                    <Text fontWeight="bold" isTruncated>
+                      {movie.title}
+                    </Text>
+                  </PseudoBox>
+                </PseudoBox>
+              </Link>
+            ))}
+          </Grid>
+          <Flex justifyContent="center">
+            <Stack isInline spacing="2">
+              {showPreviousButton && (
+                <Button
+                  variant="ghost"
+                  isDisabled={isLoading}
+                  onClick={() => setPage(page - 1)}
+                  leftIcon="arrow-back"
+                >
+                  Previous
+                </Button>
+              )}
+              {showNextButton && (
+                <Button
+                  variant="ghost"
+                  isDisabled={isLoading}
+                  onClick={() => setPage(page + 1)}
+                  rightIcon="arrow-forward"
+                >
+                  Next
+                </Button>
+              )}
+            </Stack>
+          </Flex>
+        </Stack>
+      ) : (
+        <Flex alignItems="center" justifyContent="center" flex="1">
+          {isLoading ? <Spinner /> : error && <Text>An error occurred.</Text>}
+        </Flex>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default MovieGrid;
--- src/features/movies/Movies.tsx
+++ src/features/movies/Movies.tsx
@@ -1,22 +1,7 @@
 import React, { useCallback, useState, useEffect } from "react";
-/** @jsx jsx */
-import { jsx } from "@emotion/core";
-import css from "@emotion/css/macro";
-import {
-  Flex,
-  Spinner,
-  Heading,
-  Box,
-  Grid,
-  Stack,
-  Button,
-  PseudoBox,
-  Link,
-  Text
-} from "@chakra-ui/core";
-import { Link as RouterLink } from "react-router-dom";
+import { Heading, Stack } from "@chakra-ui/core";
 import useMovies from "features/movies/useMovies";
-import TmdbImage from "components/TmdbImage";
+import MovieGrid from "features/movies/MovieGrid";
 import useQueryParam from "hooks/useQueryParam";
 
 interface MoviesProps {
@@ -51,105 +36,19 @@ const Movies: React.FC<MoviesProps> = ({ title, endpoint }) => {
     [setPage, totalPages, setPageQueryParam]
   );
 
-  return movies?.length ? (
-    <Stack spacing="4" p="4" mt="4">
+  return (
+    <Stack spacing="4" p="4" mt="4" flex="1">
       <Heading as="h1">{title}</Heading>
-      <Grid
-        gridTemplateColumns={[
-          "repeat(2, 1fr)",
-          "repeat(4, 1fr)",
-          "repeat(5, 1fr)"
-        ]}
-        gap="4"
-        alignItems="flex-start"
-      >
-        {movies.map(movie => (
-          <Link
-            // @ts-ignore
-            as={RouterLink}
-            to={`/movie/${movie.id}`}
-            key={movie.id}
-            pos="relative"
-            height="100%"
-            width="100%"
-            borderRadius="md"
-            overflow="hidden"
-            _hover={{ textDecor: "none" }}
-          >
-            <PseudoBox
-              role="group"
-              height="100%"
-              width="100%"
-              boxShadow="md"
-              transition="opacity 0.2s ease-in-out"
-              _hover={{ opacity: 0.8 }}
-            >
-              {movie.poster_path ? (
-                <TmdbImage
-                  path={movie.poster_path}
-                  imageType="poster"
-                  sizes="(max-width: 30em) 50vw, (max-width: 48em) 25vw, 20vw"
-                  css={css`
-                    width: 100%;
-                  `}
-                />
-              ) : (
-                <Box
-                  height="100%"
-                  width="100%"
-                  backgroundColor="gray.700"
-                ></Box>
-              )}
-              <PseudoBox
-                pos="absolute"
-                transform="translatey(1rem)"
-                right="0"
-                bottom="0"
-                left="0"
-                p="2"
-                pt="4"
-                backgroundImage="linear-gradient(to top, #000, transparent)"
-                opacity={0}
-                transition="transform 0.2s ease-in-out, opacity 0.2s ease-in-out"
-                _groupHover={{ transform: "translatey(0)", opacity: 1 }}
-              >
-                <Text fontWeight="bold" isTruncated>
-                  {movie.title}
-                </Text>
-              </PseudoBox>
-            </PseudoBox>
-          </Link>
-        ))}
-      </Grid>
-      <Flex justifyContent="center">
-        <Stack isInline spacing="2">
-          {totalPages && page > 1 && (
-            <Button
-              variant="ghost"
-              isDisabled={!!isFetching}
-              onClick={() => goToPage(page - 1)}
-              leftIcon="arrow-back"
-            >
-              Previous
-            </Button>
-          )}
-          {totalPages && page < totalPages && (
-            <Button
-              variant="ghost"
-              isDisabled={!!isFetching}
-              onClick={() => goToPage(page + 1)}
-              rightIcon="arrow-forward"
-            >
-              Next
-            </Button>
-          )}
-        </Stack>
-      </Flex>
+      <MovieGrid
+        movies={movies}
+        page={page}
+        setPage={goToPage}
+        showPreviousButton={page > 1}
+        showNextButton={totalPages !== null && page < totalPages}
+        isLoading={!!isFetching}
+        error={error}
+      />
     </Stack>
-  ) : (
-    <Flex alignItems="center" justifyContent="center" flex="1">
-      {isFetching ? <Spinner /> : error && <Text>An error occurred.</Text>}
-    </Flex>
   );
 };
 
