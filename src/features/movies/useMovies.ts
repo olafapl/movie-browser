f7bd40897a10ce404bd787fd342c353a5bb25a5a
Olaf Apelseth Liadal <olaf.liadal@gmail.com>
2020-04-02 21:35:59 +0200

    Simplify/improve useMovies hook

--- src/features/movies/useMovies.ts
+++ src/features/movies/useMovies.ts
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchMovies } from "features/movies/moviesSlice";
 import useSelector from "hooks/useSelector";
+import useQueryParam from "hooks/useQueryParam";
 
 /**
  * @param endpoint TMDB API endpoint to fetch movies from.
@@ -16,19 +17,30 @@ const useMovies = (
   boolean | null,
   string | null
 ] => {
-  const [page, setPage] = useState(1);
   const results = useSelector(state => state.movies[endpoint]);
   const dispatch = useDispatch();
+  const [pageParam, setPageParam] = useQueryParam("page");
+  const page = Number.parseInt(pageParam as string) || 1;
+
+  const setPage = useCallback(
+    (page: number) => {
+      setPageParam(page.toString());
+    },
+    [setPageParam]
+  );
 
   useEffect(() => {
-    if (endpoint) {
+    if (
+      endpoint &&
+      (!results || (!results.isFetching && !(page in results.pages)))
+    ) {
       dispatch(fetchMovies(endpoint, page));
     }
-  }, [endpoint, page, dispatch]);
+  }, [results, endpoint, page, dispatch]);
 
   if (results) {
     const { pages, isFetching, totalPages, error } = results;
-    return [pages[page] ?? [], page, setPage, totalPages, isFetching, error];
+    return [pages[page] ?? null, page, setPage, totalPages, isFetching, error];
   } else {
     return [null, page, setPage, null, null, null];
   }
