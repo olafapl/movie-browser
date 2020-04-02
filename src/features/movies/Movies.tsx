3f277c2021c31b37179f4b0721c25c42152ca241
Olaf Apelseth Liadal <olaf.liadal@gmail.com>
2020-04-02 21:35:59 +0200

    Simplify/improve useMovies hook

--- src/features/movies/Movies.tsx
+++ src/features/movies/Movies.tsx
@@ -1,9 +1,8 @@
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 /** @jsx jsx */
 import { jsx, Text, Container } from "theme-ui";
 import useMovies from "features/movies/useMovies";
 import MovieGrid from "features/movies/MovieGrid";
-import useQueryParam from "hooks/useQueryParam";
 
 interface MoviesProps {
   title: string;
@@ -11,31 +10,8 @@ interface MoviesProps {
 }
 
 const Movies = ({ title, endpoint }: MoviesProps) => {
-  const [pageQueryParam, setPageQueryParam] = useQueryParam("page");
   const [movies, page, setPage, totalPages, isFetching, error] = useMovies(
     endpoint
-  );
-
-  useEffect(() => {
-    const pageQueryParamValue = Number.parseInt(
-      (pageQueryParam as unknown) as string
-    );
-    if (Number.isNaN(pageQueryParamValue)) {
-      setPage(1);
-    } else if (pageQueryParamValue !== page) {
-      setPage(pageQueryParamValue);
-    }
-  }, [endpoint, page, setPage, pageQueryParam]);
-
-  const goToPage = useCallback(
-    (page: number) => {
-      if (page > 0 && totalPages && page <= totalPages) {
-        setPage(page);
-        setPageQueryParam(page.toString());
-        window.scrollTo(0, 0);
-      }
-    },
-    [setPage, totalPages, setPageQueryParam]
   );
 
   return (
@@ -46,7 +22,7 @@ const Movies = ({ title, endpoint }: MoviesProps) => {
       <MovieGrid
         movies={movies}
         page={page}
-        setPage={goToPage}
+        setPage={setPage}
         showPreviousButton={page > 1}
         showNextButton={totalPages !== null && page < totalPages}
         isLoading={!!isFetching}
