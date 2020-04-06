aab6c30a9d684a073eee5c49f68fa2b73c29cd43
Olaf Apelseth Liadal <olaf.liadal@gmail.com>
2020-04-06 15:21:17 +0200

    Simplify moviesSlice with createAsyncThunk

--- src/features/movies/moviesSlice.ts
+++ src/features/movies/moviesSlice.ts
@@ -1,116 +1,71 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { AppThunk } from "store";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { getMovies } from "api/tmdb";
+
+interface FetchArgs {
+  endpoint: string;
+  page: number;
+}
+
+export const fetchMovies = createAsyncThunk(
+  "movies/fetchMovies",
+  async ({ endpoint, page }: FetchArgs, thunkAPI) => {
+    const response = await getMovies(endpoint, page);
+    if ("results" in response) {
+      return response;
+    } else {
+      return thunkAPI.rejectWithValue(
+        `${response.status_code}: ${response.status_message}`
+      );
+    }
+  }
+);
 
 interface MoviesResults {
   pages: {
-    // pageNumber: results
     [key: number]: Tmdb.MovieResult[];
   };
   totalPages: number | null;
+}
+
+interface MoviesState {
+  endpoints: {
+    [key: string]: MoviesResults;
+  };
   isFetching: boolean;
   error: string | null;
 }
 
-interface MoviesState {
-  [key: string]: MoviesResults;
-}
-
-const initialState: MoviesState = {};
-
-const initialResultsState: MoviesResults = {
-  pages: {},
-  totalPages: null,
+const initialState: MoviesState = {
+  endpoints: {},
   isFetching: false,
-  error: null
+  error: null,
 };
-
-interface Payload {
-  endpoint: string;
-}
-
-interface MoviesPayload extends Payload {
-  results: Tmdb.PaginatedResults<Tmdb.MovieResult>;
-}
-
-interface FailedPayload extends Payload {
-  error: string;
-}
-
-interface IsFetchingPayload extends Payload {
-  isFetching: boolean;
-}
 
 const moviesSlice = createSlice({
   name: "movies",
   initialState,
-  reducers: {
-    getMoviesSuccess(state, action: PayloadAction<MoviesPayload>) {
-      const { endpoint, results } = action.payload;
-      if (!(endpoint in state)) {
-        state[endpoint] = initialResultsState;
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchMovies.pending, (state, action) => {
+      state.isFetching = true;
+    });
+    builder.addCase(fetchMovies.fulfilled, (state, action) => {
+      const response = action.payload;
+      const { endpoint, page } = action.meta.arg;
+      const { results, total_pages } = response;
+      if (!(endpoint in state.endpoints)) {
+        state.endpoints[endpoint] = { pages: {}, totalPages: null };
       }
-      state[endpoint] = {
-        ...state[endpoint],
-        totalPages: results.total_pages,
-        pages: {
-          ...state[endpoint].pages,
-          [results.page]: results.results
-        },
-        error: null
-      };
-    },
-    getMoviesFailed(state, action: PayloadAction<FailedPayload>) {
-      const { endpoint, error } = action.payload;
-      if (!(endpoint in state)) {
-        state[endpoint] = initialResultsState;
-      }
-      state[endpoint] = {
-        ...state[endpoint],
-        error
-      };
-    },
-    setIsFetching(state, action: PayloadAction<IsFetchingPayload>) {
-      const { endpoint, isFetching } = action.payload;
-      if (!(endpoint in state)) {
-        state[endpoint] = initialResultsState;
-      }
-      state[endpoint] = {
-        ...state[endpoint],
-        isFetching
-      };
-    }
-  }
+      state.endpoints[endpoint].pages[page] = results;
+      state.endpoints[endpoint].totalPages = total_pages;
+      state.isFetching = false;
+      state.error = null;
+    });
+    builder.addCase(fetchMovies.rejected, (state, action) => {
+      state.isFetching = false;
+      state.error = action.payload as string;
+    });
+  },
 });
-
-export const {
-  getMoviesSuccess,
-  getMoviesFailed,
-  setIsFetching
-} = moviesSlice.actions;
-
-export const fetchMovies = (
-  endpoint: string,
-  page: number
-): AppThunk => async dispatch => {
-  try {
-    dispatch(setIsFetching({ isFetching: true, endpoint }));
-    const results = await getMovies(endpoint, page);
-    if ("results" in results) {
-      dispatch(getMoviesSuccess({ results, endpoint }));
-    } else {
-      dispatch(
-        getMoviesFailed({
-          error: `${results.status_code}: ${results.status_message}`,
-          endpoint
-        })
-      );
-    }
-  } catch (err) {
-    dispatch(getMoviesFailed(err.toString()));
-  } finally {
-    dispatch(setIsFetching({ isFetching: false, endpoint }));
-  }
-};
 
 export default moviesSlice.reducer;
