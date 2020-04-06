e047cc8c6fe6873b0908b48f218c3ddb6c17e8ff
Olaf Apelseth Liadal <olaf.liadal@gmail.com>
2020-04-06 15:21:17 +0200

    Simplify moviesSlice with createAsyncThunk

--- src/features/movies/useMovies.ts
+++ src/features/movies/useMovies.ts
@@ -1,8 +1,8 @@
 import { useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useQueryParam, NumberParam } from "use-query-params";
 import { fetchMovies } from "features/movies/moviesSlice";
 import useSelector from "hooks/useSelector";
-import useQueryParam from "hooks/useQueryParam";
 
 /**
  * @param endpoint TMDB API endpoint to fetch movies from.
@@ -17,33 +17,32 @@ const useMovies = (
   boolean | null,
   string | null
 ] => {
-  const results = useSelector(state => state.movies[endpoint]);
+  const { endpoints, error, isFetching } = useSelector((state) => state.movies);
   const dispatch = useDispatch();
-  const [pageParam, setPageParam] = useQueryParam("page");
-  const page = Number.parseInt(pageParam as string) || 1;
-
+  const [pageParam, setPageParam] = useQueryParam("page", NumberParam);
+  const page = pageParam ?? 1;
   const setPage = useCallback(
     (page: number) => {
-      setPageParam(page.toString());
+      setPageParam(page, "pushIn");
     },
     [setPageParam]
   );
 
   useEffect(() => {
-    if (
-      endpoint &&
-      (!results || (!results.isFetching && !(page in results.pages)))
-    ) {
-      dispatch(fetchMovies(endpoint, page));
+    const pages = endpoints[endpoint]?.pages ?? {};
+    if (endpoint && !(page in pages)) {
+      dispatch(fetchMovies({ endpoint, page }));
     }
-  }, [results, endpoint, page, dispatch]);
+  }, [endpoint, page, endpoints, dispatch]);
 
-  if (results) {
-    const { pages, isFetching, totalPages, error } = results;
-    return [pages[page] ?? null, page, setPage, totalPages, isFetching, error];
-  } else {
-    return [null, page, setPage, null, null, null];
-  }
+  return [
+    endpoints?.[endpoint]?.pages[page] ?? null,
+    page,
+    setPage,
+    endpoints?.[endpoint]?.totalPages,
+    isFetching,
+    error,
+  ];
 };
 
 export default useMovies;
